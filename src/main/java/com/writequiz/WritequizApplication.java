8252b468429ee7e449870414eb12ebf8c4d8b53e
hum02 <ehdgma02@gmail>
2022-02-09 16:04:03 +0900

    Initial commit

---
+++ src/main/java/com/writequiz/WritequizApplication.java
@@ -0,0 +1,13 @@
+package com.writequiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WritequizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WritequizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/writequiz/WritequizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.writequiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WritequizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
